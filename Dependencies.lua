20607b9176b0bd7f3c0f919a94a6943d4ab69da0
Viktor Ivanov <1vvvictorivanovvv1@gmail.com>
2023-10-21 16:26:58 +0300

    Tabs

--- Dependencies.lua
+++ Dependencies.lua
@@ -1,9 +1,9 @@
 IncludeDirs = {}
 
-IncludeDirs["Glad"] 				= "%{wks.location}/Dix/Vendor/Glad/include"
-IncludeDirs["GLFW"] 				= "%{wks.location}/Dix/Vendor/GLFW/include"
-IncludeDirs["spdlog"] 				= "%{wks.location}/Dix/Vendor/spdlog/include"
-IncludeDirs["TinyObjLoader"] 		= "%{wks.location}/Dix/Vendor/TinyObjLoader/include"
-IncludeDirs["ImGui"] 				= "%{wks.location}/Dix/Vendor/ImGui"
-IncludeDirs["glm"] 					= "%{wks.location}/Dix/Vendor/glm"
-IncludeDirs["stb_image"] 			= "%{wks.location}/Dix/Vendor/stb_image/include"
+IncludeDirs["Glad"]				= "%{wks.location}/Dix/Vendor/Glad/include"
+IncludeDirs["GLFW"]				= "%{wks.location}/Dix/Vendor/GLFW/include"
+IncludeDirs["spdlog"]			= "%{wks.location}/Dix/Vendor/spdlog/include"
+IncludeDirs["TinyObjLoader"]	= "%{wks.location}/Dix/Vendor/TinyObjLoader/include"
+IncludeDirs["ImGui"]			= "%{wks.location}/Dix/Vendor/ImGui"
+IncludeDirs["glm"]				= "%{wks.location}/Dix/Vendor/glm"
+IncludeDirs["stb_image"]		= "%{wks.location}/Dix/Vendor/stb_image/include"
