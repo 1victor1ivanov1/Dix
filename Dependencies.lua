c86d3ecd4c6e2653c6859a9910901c6fa04e354b
Viktor Ivanov <1vvvictorivanovvv1@gmail.com>
2023-10-21 15:11:37 +0300

    Updated other lua scripts

--- Dependencies.lua
+++ Dependencies.lua
@@ -1,8 +1,9 @@
 IncludeDirs = {}
 
-IncludeDirs["Glad"] 		= "%{wks.location}/Dix/Vendor/Glad/include"
-IncludeDirs["GLFW"] 		= "%{wks.location}/Dix/Vendor/GLFW/include"
-IncludeDirs["spdlog"] 		= "%{wks.location}/Dix/Vendor/spdlog/include"
-IncludeDirs["assimp"] 		= "%{wks.location}/Dix/Vendor/assimp/include"
-IncludeDirs["glm"] 			= "%{wks.location}/Dix/Vendor/glm"
-IncludeDirs["stb_image"] 	= "%{wks.location}/Dix/Vendor/stb_image"
+IncludeDirs["Glad"] 				= "%{wks.location}/Dix/Vendor/Glad/include"
+IncludeDirs["GLFW"] 				= "%{wks.location}/Dix/Vendor/GLFW/include"
+IncludeDirs["spdlog"] 				= "%{wks.location}/Dix/Vendor/spdlog/include"
+IncludeDirs["TinyObjLoader"] 		= "%{wks.location}/Dix/Vendor/TinyObjLoader/include"
+IncludeDirs["ImGui"] 				= "%{wks.location}/Dix/Vendor/ImGui"
+IncludeDirs["glm"] 					= "%{wks.location}/Dix/Vendor/glm"
+IncludeDirs["stb_image"] 			= "%{wks.location}/Dix/Vendor/stb_image"
